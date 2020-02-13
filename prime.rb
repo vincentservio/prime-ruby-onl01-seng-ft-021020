b96ecbe2ce4e0f0e1968783014df730eae2a881c
Vincent Servio <vincentservio@gmail.com>
2020-02-13 16:18:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,8 @@
 # Add  code here!
 require "pry"
 def prime?(i)
-  if i <= 1 
-    return false 
-  elsif i <= 3 
-  return true 
-else (2..i/2).none? do |x|
+ (2..i/2).none? do |x|
   i % x == 0
-end
+
 end 
 end 
